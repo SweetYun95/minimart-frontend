527bcf20f931576df087e2fbf88f0a36406c9b23
SweetYun95 <isr01154@naver.com>
2025-08-04 12:38:47 +0900

    [fix] App.jsx 파일 주석처리

--- shopmax-frontend_dev/src/App.jsx
+++ shopmax-frontend_dev/src/App.jsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
 
 import Footer from './components/shared/Footer'
-import Home from './pages/Home'
-import SignupPage from './pages/SignupPage'
-import LoginPage from './pages/LoginPage'
+// import Home from './pages/Home'
+// import SignupPage from './pages/SignupPage'
+// import LoginPage from './pages/LoginPage'
 
 import Navbar from './components/shared/Navber'
 
@@ -14,9 +14,9 @@ function App() {
       <>
          <Navbar />
          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/login" element={<LoginPage />} />
+            {/* <Route path="/" element={<Home />} /> */}
+            {/* <Route path="/signup" element={<SignupPage />} /> */}
+            {/* <Route path="/login" element={<LoginPage />} /> */}
          </Routes>
          <Footer />
       </>
