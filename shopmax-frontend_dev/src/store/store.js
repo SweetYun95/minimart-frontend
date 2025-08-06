cfae641e45b897a2e37133c4e1bff9a7b86288b3
SweetYun95 <isr01154@naver.com>
2025-08-06 10:57:37 +0900

    [feat]주문 관련 기능 추가 Api,Slice,store

--- shopmax-frontend_dev/src/store/store.js
+++ shopmax-frontend_dev/src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/authSlice'
 import itemReducer from '../features/itemSlice'
 import tokenReducer from '../features/tokenSlice'
+import orderReducer from '../features/orderSlice' // 주문 slice 추가
 
 const store = configureStore({
    reducer: {
       auth: authReducer,
       item: itemReducer,
       token: tokenReducer,
+      order: orderReducer, // 주문 slice 등록
    },
 })
 
