e76760d3b2bd9d87c04e29f7b0dbf735bbd87939
dtkd19 <tkdgns9804@naver.com>
2025-08-07 15:25:02 +0900

    [feat]장바구니페이지 추가

---
+++ shopmax-frontend_dev/src/api/cartApi.js
@@ -0,0 +1,29 @@
+import shopmaxApi from './axiosApi'
+
+// 장바구니 전체 조회
+export const getCartItems = async () => {
+   const res = await shopmaxApi.get('/cart', {
+      withCredentials: true,
+   })
+   return res.data
+}
+
+// 장바구니에 상품 추가
+export const addToCart = async ({ itemId, count }) => {
+   const res = await shopmaxApi.post('/cart/add', { itemId, count }, { withCredentials: true })
+   return res.data
+}
+
+// 장바구니 수량 수정
+export const updateCartItem = async ({ cartItemId, count }) => {
+   const res = await shopmaxApi.put(`/cart/update/${cartItemId}`, { count }, { withCredentials: true })
+   return res.data
+}
+
+// 장바구니 상품 삭제
+export const deleteCartItem = async (cartItemId) => {
+   const res = await shopmaxApi.delete(`/cart/delete/${cartItemId}`, {
+      withCredentials: true,
+   })
+   return res.data
+}
--- shopmax-frontend_dev/src/features/likeSlice.js
+++ shopmax-frontend_dev/src/features/likeSlice.js
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-
 const savedLikes = JSON.parse(localStorage.getItem('likes')) || {}
 
 const likeSlice = createSlice({
