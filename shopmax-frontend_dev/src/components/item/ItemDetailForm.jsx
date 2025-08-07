8d5772213946900b9890048882ab6c18f5a44b97
eeinoeyes <loveourjune@gmail.com>
2025-08-07 10:48:18 +0900

    [feat] ItemDetailForm.jsx 상품이미지 출력 기능 구현

--- shopmax-frontend_dev/src/components/item/ItemDetailForm.jsx
+++ shopmax-frontend_dev/src/components/item/ItemDetailForm.jsx
@@ -1,5 +1,5 @@
 // src/components/item/ItemDetailForm.jsx
-import { Box, Button, Typography, Stack, TextField } from '@mui/material'
+import { Box, Button, Typography, Stack, TextField, keyframes } from '@mui/material'
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 
@@ -17,6 +17,8 @@ function ItemDetailForm({ item }) {
       const value = Math.max(1, Number(e.target.value) || 1)
       setQuantity(value)
    }
+
+   console.log(item.ItemImages.filter((data) => data.repImgYn === 'N').map((img) => img.imgUrl))
 
    const handleBuyNow = async () => {
       try {
@@ -44,40 +46,74 @@ function ItemDetailForm({ item }) {
    }
 
    return (
-      <Box>
-         {/* 상품 이름 */}
-         <Typography variant="h4" gutterBottom>
-            {item.itemNm}
-         </Typography>
+      <>
+         {/* 이미지 출력 박스 */}
+         <Box
+            sx={{
+               display: 'flex',
+            }}
+         >
+            {/* 서브 이미지 */}
+            <Box
+               sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  width: '90px',
+                  height: '104px',
+               }}
+            >
+               {item.ItemImages.filter((data) => data.repImgYn === 'N').map((img, index) => (
+                  <img src={`${import.meta.env.VITE_APP_API_URL}${img.imgUrl}`} key={index} />
+               ))}
+            </Box>
+            {/* 대표 이미지 */}
+            <Box>
+               <img
+                  src={`${import.meta.env.VITE_APP_API_URL}${item.ItemImages.filter((img) => img.repImgYn === 'Y')[0].imgUrl}`}
+                  sx={{
+                     width: '540px',
+                     height: '622px',
+                  }}
+               />
+            </Box>
+         </Box>
 
-         {/* 상품 가격 */}
-         <Typography variant="h6" gutterBottom>
-            {item.price.toLocaleString()} 원
-         </Typography>
-
-         {/* 간단 설명 */}
-         {item.itemDetail && (
-            <Typography variant="body1" gutterBottom>
-               {item.itemDetail}
+         {/* 상품 정보 출력 박스 */}
+         <Box>
+            {/* 상품 이름 */}
+            <Typography variant="h4" gutterBottom>
+               {item.itemNm}
             </Typography>
-         )}
 
-         {/* 수량 선택 */}
-         <Stack direction="row" spacing={2} alignItems="center" sx={{ my: 2 }}>
-            <TextField label="수량" type="number" value={quantity} onChange={handleQuantityChange} InputProps={{ inputProps: { min: 1 } }} size="small" sx={{ width: '100px' }} />
-            <Typography>총 {(item.price * quantity).toLocaleString()} 원</Typography>
-         </Stack>
+            {/* 상품 가격 */}
+            <Typography variant="h6" gutterBottom>
+               {item.price.toLocaleString()} 원
+            </Typography>
 
-         {/* 버튼 영역 */}
-         <Stack direction="row" spacing={2}>
-            <Button variant="outlined" onClick={handleAddToCart} fullWidth>
-               장바구니
-            </Button>
-            <Button variant="contained" color="primary" onClick={handleBuyNow} fullWidth>
-               구매하기
-            </Button>
-         </Stack>
-      </Box>
+            {/* 간단 설명 */}
+            {item.itemDetail && (
+               <Typography variant="body1" gutterBottom>
+                  {item.itemDetail}
+               </Typography>
+            )}
+
+            {/* 수량 선택 */}
+            <Stack direction="row" spacing={2} alignItems="center" sx={{ my: 2 }}>
+               <TextField label="수량" type="number" value={quantity} onChange={handleQuantityChange} InputProps={{ inputProps: { min: 1 } }} size="small" sx={{ width: '100px' }} />
+               <Typography>총 {(item.price * quantity).toLocaleString()} 원</Typography>
+            </Stack>
+
+            {/* 버튼 영역 */}
+            <Stack direction="row" spacing={2}>
+               <Button variant="outlined" onClick={handleAddToCart} fullWidth>
+                  장바구니
+               </Button>
+               <Button variant="contained" color="primary" onClick={handleBuyNow} fullWidth>
+                  구매하기
+               </Button>
+            </Stack>
+         </Box>
+      </>
    )
 }
 
