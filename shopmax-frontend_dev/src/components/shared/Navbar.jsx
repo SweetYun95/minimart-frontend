0d214c4d466aec6c999dc5683355be19f4302bdd
SweetYun95 <isr01154@naver.com>
2025-08-08 10:38:56 +0900

    [patch]회원가입시 전화번호 하이폰 자동 추가

--- shopmax-frontend_dev/src/components/shared/Navbar.jsx
+++ shopmax-frontend_dev/src/components/shared/Navbar.jsx
@@ -7,60 +7,75 @@ import Container from '@mui/material/Container'
 
 import '../css/Navbar.css'
 
-
 function Navbar() {
    return (
-      <AppBar position="fixed" color='transparent' sx={{ backgroundColor: 'transparent', color: '#000', boxShadow: 'none'}}>
- 
+      <AppBar position="fixed" color="transparent" sx={{ backgroundColor: 'transparent', color: '#000', boxShadow: 'none' }}>
          <Container maxWidth="xl">
-            <Toolbar sx={{ margin: '0 auto',justifyContent: 'space-between', maxWidth: "1200px" }}>
-                <NavLink to='/' className="galindo logo">
-                     PETHAUL
-                  </NavLink>
+            <Toolbar sx={{ margin: '0 auto', justifyContent: 'space-between', maxWidth: '1200px' }}>
+               <NavLink to="/" className="galindo logo">
+                  PETHAUL
+               </NavLink>
                <ul>
                   <li>
-                      <NavLink>MENU</NavLink>
+                     <NavLink>MENU</NavLink>
                   </li>
                   <li>
-                      <NavLink>SEASON<iconify-icon style={{marginLeft: '5px'}} icon="fluent-emoji-flat:watermelon" width="16
-                  " height="16
-                  "></iconify-icon></NavLink>
+                     <NavLink>
+                        SEASON
+                        <iconify-icon
+                           style={{ marginLeft: '5px' }}
+                           icon="fluent-emoji-flat:watermelon"
+                           width="16
+                  "
+                           height="16
+                  "
+                        ></iconify-icon>
+                     </NavLink>
                   </li>
                   <li>
-                     <NavLink >이벤트/기획전<iconify-icon style={{marginLeft: '5px'}} icon="fluent-emoji:star" width="16
-                  " height="16
-                  "></iconify-icon></NavLink></li>
-                <li>
-                  <NavLink >고객센터</NavLink>
-                </li>
+                     <NavLink>
+                        이벤트/기획전
+                        <iconify-icon
+                           style={{ marginLeft: '5px' }}
+                           icon="fluent-emoji:star"
+                           width="16
+                  "
+                           height="16
+                  "
+                        ></iconify-icon>
+                     </NavLink>
+                  </li>
+                  <li>
+                     <NavLink>고객센터</NavLink>
+                  </li>
                </ul>
-               <div className='right-icon-bar'>
-               <div className='pc-search-icon search'>
-                  <IconButton className=''>
-                     <iconify-icon icon="pixelarticons:search" width="24" height="24"></iconify-icon>
-                  </IconButton>
-               </div>
-               <div className='mob-search-icon search'>
-                  <IconButton >
-                     <iconify-icon  icon="pixelarticons:search" width="28" height="28"></iconify-icon>
-                  </IconButton>
-               </div>
+               <div className="right-icon-bar">
+                  <div className="pc-search-icon search">
+                     <IconButton className="">
+                        <iconify-icon icon="pixelarticons:search" width="24" height="24"></iconify-icon>
+                     </IconButton>
+                  </div>
+                  <div className="mob-search-icon search">
+                     <IconButton>
+                        <iconify-icon icon="pixelarticons:search" width="28" height="28"></iconify-icon>
+                     </IconButton>
+                  </div>
 
-               {/* 오른쪽 아이콘 영역 */}
-               <div className='icon'>
-                  <IconButton>
-                     <iconify-icon icon="pixelarticons:heart" width="24" height="24"></iconify-icon>
-                  </IconButton>
-                  <IconButton>
-                     <iconify-icon icon="streamline-pixel:shopping-shipping-basket" width="24" height="24"></iconify-icon>
-                  </IconButton>
-                  <IconButton>
-                     <iconify-icon icon="streamline-pixel:user-single-aim" width="24" height="24"></iconify-icon>
-                  </IconButton>
-               </div>
-               <div className='mobile-menu'>
-                  <iconify-icon icon="streamline-pixel:interface-essential-navigation-menu-3" width="35" height="35"></iconify-icon>
-               </div>
+                  {/* 오른쪽 아이콘 영역 */}
+                  <div className="icon">
+                     <IconButton>
+                        <iconify-icon icon="pixelarticons:heart" width="24" height="24"></iconify-icon>
+                     </IconButton>
+                     <IconButton>
+                        <iconify-icon icon="streamline-pixel:shopping-shipping-basket" width="24" height="24"></iconify-icon>
+                     </IconButton>
+                     <IconButton>
+                        <iconify-icon icon="streamline-pixel:user-single-aim" width="24" height="24"></iconify-icon>
+                     </IconButton>
+                  </div>
+                  <div className="mobile-menu">
+                     <iconify-icon icon="streamline-pixel:interface-essential-navigation-menu-3" width="35" height="35"></iconify-icon>
+                  </div>
                </div>
             </Toolbar>
          </Container>
