7a3f91cd0a87af51c492b15c9fc7680ff6377874
eeinoeyes <loveourjune@gmail.com>
2025-08-08 11:37:31 +0900

    [feat] 상품 관련 컴포넌트 수정-카테고리 테이블값 반영

--- shopmax-frontend_dev/src/pages/ItemEditPage.jsx
+++ shopmax-frontend_dev/src/pages/ItemEditPage.jsx
@@ -1,20 +1,30 @@
 import { Container } from '@mui/material'
 
-import { useLocation } from 'react-router-dom'
-
 import ItemEditForm from '../components/item/ItemEditForm'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { fetchItemByIdThunk } from '../features/itemSlice'
 
 function ItemEditPage() {
-   const location = useLocation()
-   const { initialData } = location.state || {}
+   const dispatch = useDispatch()
+   const { id } = useParams()
+   const { item, loading, error } = useSelector((state) => state.item)
 
-   console.log(initialData)
+   useEffect(() => {
+      dispatch(fetchItemByIdThunk(id))
+   }, [dispatch, id])
 
+   if (!item) return <p>상품이 존재하지 않습니다.</p>
+   if (loading) return <p>로딩 중...</p>
+   if (error) return <p>에러 발생:{error}</p>
+
+   console.log(item)
    return (
       <>
          <Container maxWidth="md" sx={{ marginTop: 10, marginBottom: 13 }}>
             <h1>상품 수정</h1>
-            <ItemEditForm initialData={initialData} />
+            <ItemEditForm initialData={item} />
          </Container>
       </>
    )
